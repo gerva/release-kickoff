712da24aae78024bb232ed06b8ef6fcd1ecee16b
Massimo Gervasini <mgervasini@mozilla.com>
2013-06-11 11:21:50 +0200

    work in progress: fixing release url js code

--- kickoff/static/kickoff.js
+++ kickoff/static/kickoff.js
@@ -88,7 +88,7 @@ function submit_form_manager(){
        .blur( function(){ revisionUrlBlur(product) });
       $( "#" + product + "\-mozillaRevision" )
        .blur( function(){ revisionBlur(product) });
-      $( "#" + product + "\-mozillaRelBranch")
+      $( "#" + product + "\-mozillaRelbranch")
        .blur( function(){ relBranchBlur(product) });
       // preserve the state after a refresh
       var lb = getLastBlurredItem(product)
@@ -100,26 +100,23 @@ function submit_form_manager(){
 }
 
 function revisionUrlBlur(product) {
-  /* regex for revision */
   var url = getRevisionUrl(product)
-  var regex = /https:\/\/hg.mozilla.org\/(.*)\/rev\/[0-9A-Fa-f]{12}/;
-  var match = regex.exec( url );
-  if ( match ) {
-    setBranch(product, match[1])
-    setRevision(product, match[2])
+  var branch = getBranchName(url)
+  var revision = getResource(url)
+  if ( isValidRevision(revision) ) {
+    setBranch(product, branch)
+    enableRevision(product)
     disableRelBranch(product)
-    setLastBlurredItem(product, "revision-url")
-    return
-  }
-  var regex = /https:\/\/hg.mozilla.org\/(.*)\/rev\/(.*)/;
-  var match = regex.exec( url );
-  if ( match ) {
-    setBranch(product, match[1])
-    setReleaseBranch(product, match[2])
+    setRevision(product, revision)
+    setReleaseBranch(product, "")
+  } else {
+    setBranch(product, branch)
+    enableRelBranch(product)
     disableRevision(product)
-    setLastBlurredItem(product, "revision-url")
-    return
+    setReleaseBranch(product, revision)
+    setRevision(product, "")
   }
+  setLastBlurredItem(product, "revision-url")
 }
 
 function relBranchBlur(product) {
@@ -149,7 +146,7 @@ function revisionBlur(product) {
         setRevisionUrl(product, url)
       }
     }
-    setLastBlurredItem(product, "mozillaRelBranch")
+    setLastBlurredItem(product, "mozillaRelbranch")
 }
 
 function branchBlur(product) {
@@ -168,7 +165,28 @@ function branchBlur(product) {
 
 
 /* a bunch of gettes/setters to make code more readable */
-function enableFormElement(elment_id) {
+function isValidRevision(revisionId) {
+  return /[a-fA-F][0-9]{12}/.exec(revisionId)
+}
+
+function getResource(urlstring) {
+  var regex = "^.*/rev/\(.*\)$"
+  if ( match ) {
+    return match[0]
+  }
+  return ""
+}
+
+function getBranchName(urlstring) {
+  var regex = "^.*/\(.*\)/rev/.*$"
+  var match = regex.exec(urlstring)
+  if ( match ) {
+    return match[0]
+  }
+  return ""
+}
+
+function enableFormElement(element_id) {
   $( "#" + element_id + ":disabled" ).removeAttr("disabled")
 }
 
@@ -177,7 +195,7 @@ function disableFormElement(element_id) {
 }
 
 function disableRelBranch(release_type) {
-   disableFormElement( release_type + "-mozillaRelBranch" )
+   disableFormElement( release_type + "-mozillaRelbranch" )
 }
 
 function disableRevision(release_type) {
@@ -185,51 +203,51 @@ function disableRevision(release_type) {
 }
 
 function enableRelBranch(release_type) {
-  enableFormElement( release_type + "-mozillaRelBranch" )
+  enableFormElement( release_type + "-mozillaRelbranch" )
 }
 
 function enableRevision(release_type) {
   enableFormElement( release_type + "-mozillaRevision" )
 }
 
-function setFormInputElment(element_id, value) {
+function setFormInputElement(element_id, value) {
   $( "#" + element_id ).val( value )
 }
 
-function getFormInputElment(element_id) {
+function getFormInputElement(element_id) {
   return $( "#" + element_id ).val().trim()
 }
 
 function getRevision(release_type) {
-  return getFormInputElment( release_type + "-mozillaRevision" )
+  return getFormInputElement( release_type + "-mozillaRevision" )
 }
 
 function setRevision(release_type, value) {
-  setFormInputElment( release_type + "-mozillaRevision", value )
+  setFormInputElement( release_type + "-mozillaRevision", value )
 }
 
 function getRevisionUrl(release_type) {
-  return getFormInputElment( release_type + "-revision-url" )
+  return getFormInputElement( release_type + "-revision-url" )
 }
 
 function setRevisionUrl(release_type, value) {
-  setFormInputElment( release_type + "-revision-url" , value )
+  setFormInputElement( release_type + "-revision-url" , value )
 }
 
 function getReleaseBranch(release_type) {
-  return getFormInputElment( release_type + "-mozillaRelBranch" )
+  return getFormInputElement( release_type + "-mozillaRelbranch" )
 }
 
 function setReleaseBranch(release_type, value) {
-  setFormInputElment( release_type + "-mozillaRelBranch", value )
+  setFormInputElement( release_type + "-mozillaRelbranch", value )
 }
 
 function getBranch(release_type) {
-  return getFormInputElment( release_type + "-branch" )
+  return getFormInputElement( release_type + "-branch" )
 }
 
 function setBranch(release_type, value) {
-  setFormInputElment( release_type + "-branch", value )
+  setFormInputElement( release_type + "-branch", value )
 }
 
 function setLastBlurredItem(release_type, name) {
